54089135c0cff95a60a89275d848f79507547c9f
hsuntarq <hsuntariq@gmail.com>
2023-04-25 00:04:07 +0500

    middlewares

---
+++ middlewares/errorMiddleware.js
@@ -0,0 +1,9 @@
+const errorHandler = (err,req, res, next) => {
+    const statusCode = res.statusCode ? res.statusCode : 500;
+    res.status(statusCode);
+    res.json({
+        error: err.message
+    })
+}
+
+module.exports = { errorHandler };
