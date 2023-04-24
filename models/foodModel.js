55f49a79cb4f92ab0a95439212d37d1694ccd3af
hsuntarq <hsuntariq@gmail.com>
2023-04-25 00:04:31 +0500

    models

---
+++ models/foodModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const foodSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,'please enter the dish name'],
+    },
+    price:{
+        type:mongoose.Types.Decimal128,
+        required:[true,'please enter the dish name'],
+    },
+    info: {
+        type: String,
+        required:[true,'please enter the dish info']
+    },
+    image: {
+        type: String,
+        required: false,
+        default: 'https://cdn-icons-png.flaticon.com/512/1377/1377194.png',
+    },
+})
+
+module.exports = mongoose.model('Food', foodSchema);
