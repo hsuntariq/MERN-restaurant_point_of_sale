a79ecf3b9d58e50da7e9383992efd514b7d3b7f0
hsuntarq <hsuntariq@gmail.com>
2023-04-25 15:20:51 +0500

    backend

---
+++ backend/controller/foodController.js
@@ -0,0 +1,51 @@
+const Food = require("../models/foodModel");
+const AsyncHandler = require('express-async-handler')
+const getFood = AsyncHandler(async(req, res) => {
+    const food = await Food.find();
+    res.status(200).json(food);
+});
+
+const postFood =AsyncHandler(async(req, res) => {
+    if (!req.body.name || !req.body.price || !req.body.info) {
+        res.status(400);
+        throw new Error('please fill out all the fields')
+    }
+    // get the info from the body
+    const { name, price, info, image } = req.body;
+    const food = await Food.create({
+        name, price, info, image
+    });
+    res.status(200).json(food);
+});
+
+const updateFood = async(req, res) => {
+    const findFood = await Food.findById(req.params.id);
+    if (!findFood) {
+        res.status(404);
+        throw new Error('no item with the given id');
+    }
+    const updatedItem = await Food.findByIdAndUpdate(findFood, req.body, {
+        new: true,
+    });
+    res.status(200).json(updatedItem);
+};
+const deleteFood = AsyncHandler(async(req, res) => {
+    const findFood = await Food.findById(req.params.id);
+    if (!findFood) {
+        res.status(404);
+        throw new Error('id does not exist');
+    }
+    await findFood.deleteOne();
+    res.status(200).json({
+        _id: findFood._id,
+    })
+});
+
+
+
+module.exports = {
+    getFood,
+    postFood,
+    updateFood,
+    deleteFood
+}
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,17 @@
-console.log('hello')
+require('colors');
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('../config/connect');
+const { errorHandler } = require('../middlewares/errorMiddleware');
+const port = process.env.PORT || 5000;
+const app = express();
+connectDB()
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/dish',require('../routes/foodRoutes'))
+
+app.use(errorHandler);
+
+app.listen(port, ()=>console.log(`server started on port: ${port.cyan}`))
+
