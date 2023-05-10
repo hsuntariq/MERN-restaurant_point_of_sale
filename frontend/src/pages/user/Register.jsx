61a94f232400b62a9213b06457b42444ba072ca8
hsuntarq <hsuntariq@gmail.com>
2023-05-11 00:12:14 +0500

    registration form

--- frontend/src/pages/user/Register.jsx
+++ frontend/src/pages/user/Register.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
-
+import { Container,Form } from 'react-bootstrap';
 const Register = () => {
 return (
     <>
-        <h1>Register</h1>
+        <Container>
+            <Form>
+                <Form.Group>
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control type="text" placeholder="Please enter your name..."/>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="email" placeholder="Please enter your email..."/>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="text" placeholder="Your password..."/>
+                </Form.Group>
+            </Form>
+        </Container>
     </>
 )
 }
